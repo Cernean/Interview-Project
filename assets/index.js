3403b13892f7c3e592abbbf0b5a039220bc9580c
Cernean <ccernean@yahoo.com>
2022-10-23 20:09:10 -0400

    finish

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+// console.log("Hi")
+let header = document.querySelector ('#Ral')
+console.log(header)
+let image = document.querySelector('img')
+console.log(image)
+let mainHeader = document.querySelector('h1')
+console.log(mainHeader)
